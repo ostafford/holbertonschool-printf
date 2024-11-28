461d55bcd63b1ca40c161524b608f3761871be9f
Okky Stafford <142310611+ostafford@users.noreply.github.com>
2024-11-28 15:27:33 +1100

    Added print_digit function in print_function.c file

--- print_punctions.c
+++ print_punctions.c
@@ -42,3 +42,27 @@ int print_percent(void)
 _putchar('%');
 return (1);
 }
+
+/**            
+ * print_digit 
+ *
+ * Return:
+ */
+int print_digit(long n, int base)
+{
+int count;
+char *symbols;
+symbols = "0123456789";
+if (n < 0)
+{
+write (1, "-", 1);
+return print_digit(-n, base) +1;
+}
+else if (n < base)
+return print_char(symbols[n]);
+else
+{
+count = print_digit(n / base, base);
+return count + print_digit(n % base, base);
+}
+}
