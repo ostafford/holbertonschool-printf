91c957e2429cc8df39a28681a6d1b7324c350705
rico <10099@holbertonstudents.com>
2024-11-28 03:28:39 +0000

    seems to work

--- main.c
+++ main.c
@@ -12,7 +12,7 @@ int main(void)
   char array[] = "doo doo";
 
   _printf("Hello %s\n", "John");
-  _printf("This shit %s working? %s\n", array, "ahhahahahhaha");
+  _printf("Easy, Easy %d, what's good gangsta %s\n", 215, array);
   _printf("%d\n", 123123123);
   
   return (0); 
