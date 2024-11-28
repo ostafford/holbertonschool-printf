7db42072c778d6637b7457762ee96b947c55470f
Okky Stafford <142310611+ostafford@users.noreply.github.com>
2024-11-28 11:02:52 +1100

    added print line in ln31

--- _printf.c
+++ _printf.c
@@ -16,7 +16,7 @@ int _printf(const char *format, ...)
     va_list args;
     int i = 0, count = 0;
 
-    if (!format)
+    if (!format || *format == '\0')
         return (-1);  
 
     va_start(args, format);
